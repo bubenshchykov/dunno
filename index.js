8d30463c1fe9d9c00092ebb3d5571f70b257cbba
bubenshchykov <a.bubenshchykov@gmail.com>
2015-08-01 03:58:06 +0800

    better output

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+/* https://github.com/bubenshchykov/dunno */
+
 (function(exports) {
 
 	var key = 'dunno#v0.0.0';
-	var mask = '<:tag cl=":class" id=":id">:info</:tag>';
+	var mask = '<:tag id=":id" cl=":class">:info</:tag>';
 	var limit = 20;
+	var urlcheck = 100;
 	var started, href;
 
 	exports.start = function(opts) {
+		if (started) return;
+		started = true
+		
 		opts = opts || {};
 		opts.limit = opts.limit || limit;
+		opts.urlcheck = opts.urlcheck || urlcheck;
 
 		var push = function(entry) {
 			var rec = window.localStorage.getItem(key) || '';
@@ -23,22 +30,27 @@
 		};
 
 		var track = function() {
-			if (started) return;
-			started = true
 			
 			setInterval(function() {
 				if (window.location.href === href) return;
 				href = window.location.href;
 				locate();
-			}, 100);
+			}, urlcheck);
 			
 			document.onclick = function(e) {
 				var el = e.target;
+				
+				var tag = String(el.tagName || '').toLowerCase().slice(0, 10);
+				var id = String(el.id || '').slice(0, 10);
+				var cl = String(el.className || '').slice(0, 30);
+				var info = String(el.innerText || el.innerHTML || '').slice(0, 20).replace(/(?:\r\n|\r|\n)/g, ' ');
+				
 				var entry = mask
-					.replace(/:tag/g, el.tagName.toLowerCase())
-					.replace(':class', el.className).replace(' cl=""', '')
-					.replace(':info', el.innerText.slice(0,10))
-					.replace(':id', el.id).replace(' id=""', '');
+					.replace(/:tag/g, tag)
+					.replace(':id', id).replace(' id=""', '')
+					.replace(':class', cl).replace(' cl=""', '')
+					.replace(':info', info);
+					
 				push('click: ' + entry);
 			};
 
@@ -51,9 +63,9 @@
 			};
 		};
 
-		document.readyState === 'complete' ?
-			track() :
-			document.addEventListener('DOMContentLoaded', track);
+		['complete', 'interactive'].indexOf(document.readyState) === -1 ?
+			document.addEventListener('DOMContentLoaded', track) :
+			track();
 	};
 	
 	exports.tell = function(){
