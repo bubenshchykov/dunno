dc456495bc19e91f21a232d5842cb4676c073c73
bubenshchykov <a.bubenshchykov@gmail.com>
2015-08-02 21:52:37 +0800

    added spec

---
+++ dunno.spec.js
@@ -0,0 +1,32 @@
+var browser = require('zombie').create();
+
+var expected = 
+	'click: <a cl="error">now, click on me - I</a> ~ ' +
+	'url: /Users/alex/git/bub/dunno/demo.html?jumped=elsewhere ~ ' +
+	'click: <i cl="away">click to navigate</i> ~ ' +
+	'click: <textarea cl="bro">bro, click me too</textarea> ~ ' +
+	'click: <button cl="yo">yo, click me too</button> ~ ' +
+	'url: /Users/alex/git/bub/dunno/demo.html ~';
+
+browser
+	.visit('file://' + __dirname + '/example.html')
+	.done(function() {
+		browser.assert.text('title', 'dunno');
+		browser.fire('.yo', 'click').done(function() {
+			browser.fire('.bro', 'click').done(function() {
+				browser.fire('.away', 'click').done(function() {
+					browser.wait(200).done(function() {
+						browser.fire('.error', 'click').done(function() {
+							browser.wait(200).done(function() {
+								browser.fire('.check', 'click').done(function() {
+									browser.assert.text('.report', expected);
+									console.log('test passed');
+									process.exit(0);
+								});
+							});
+						});
+					});
+				});	
+			});
+		});
+	});
