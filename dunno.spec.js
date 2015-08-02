63418d352489056865d3bae093b02839ddcefbad
bubenshchykov <a.bubenshchykov@gmail.com>
2015-08-02 22:11:51 +0800

    better spec

--- dunno.spec.js
+++ dunno.spec.js
@@ -1,32 +1,28 @@
 var browser = require('zombie').create();
 
-var expected = 
-	'click: <a cl="error">now, click on me - I</a> ~ ' +
-	'url: /Users/alex/git/bub/dunno/example.html?jumped=elsewhere ~ ' +
-	'click: <i cl="away">click to navigate</i> ~ ' +
-	'click: <textarea cl="bro">bro, click me too</textarea> ~ ' +
-	'click: <button cl="yo">yo, click me too</button> ~ ' +
-	'url: /Users/alex/git/bub/dunno/example.html ~';
-
 browser
 	.visit('file://' + __dirname + '/example.html')
 	.done(function() {
+		
 		browser.assert.text('title', 'dunno');
 		browser.fire('.yo', 'click').done(function() {
-			browser.fire('.bro', 'click').done(function() {
-				browser.fire('.away', 'click').done(function() {
-					browser.wait(200).done(function() {
-						browser.fire('.error', 'click').done(function() {
-							browser.wait(200).done(function() {
-								browser.fire('.check', 'click').done(function() {
-									browser.assert.text('.report', expected);
-									console.log('test passed');
-									process.exit(0);
-								});
-							});
-						});
-					});
-				});	
-			});
-		});
-	});
+		browser.fire('.bro', 'click').done(function() {
+		browser.fire('.away', 'click').done(function() {
+		browser.wait(200).done(function() {
+		browser.fire('.error', 'click').done(function() {
+		browser.wait(200).done(function() {
+		browser.fire('.check', 'click').done(function() {
+			
+			browser.assert.text('.report',
+				
+				'click: <a cl="error">now, click on me - I</a> ~ ' +
+				'url: /Users/alex/git/bub/dunno/example.html?jumped=elsewhere ~ ' +
+				'click: <i cl="away">click to navigate</i> ~ ' +
+				'click: <textarea cl="bro">bro, click me too</textarea> ~ ' +
+				'click: <button cl="yo">yo, click me too</button> ~ ' +
+				'url: /Users/alex/git/bub/dunno/example.html ~');
+			
+			console.log('test passed');
+			process.exit(0);
+
+	})})})})})})})});
